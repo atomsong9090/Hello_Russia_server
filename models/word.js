68ddb0c4858deb7e4d50c5bb61770ff58949bb5a
atomsong92 <yesparrot@gmail.com>
2021-03-28 22:42:04 +0900

    sequelize data setting

---
+++ models/word.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Word extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Word.init(
+    {
+      ru: DataTypes.STRING,
+      en: DataTypes.STRING,
+      example: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Word",
+      timestamps: false,
+    }
+  );
+  return Word;
+};
